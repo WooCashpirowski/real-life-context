69462b8747f87410e11955ddea730ddaf7d4151e
woo_cashpirowski <cashpirowski@gmail.com>
2024-01-07 14:18:30 +0100

    10.002 | add survey link - mobile

--- components/Header/Sidebar/Sidebar.js
+++ components/Header/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import useTranslation from 'next-translate/useTranslation';
 import LangSwitcher from '../LangSwitcher/LangSwitcher';
 
 const Sidebar = ({ isExpanded, setIsExpanded }) => {
-	let { t } = useTranslation();
+	const { t } = useTranslation('home');
 	return (
 		<SidebarStyled className={isExpanded ? 'expanded' : ''}>
 			<ul>
@@ -14,27 +14,36 @@ const Sidebar = ({ isExpanded, setIsExpanded }) => {
 					smooth
 					onClick={() => setIsExpanded(!isExpanded)}
 				>
-					<li>{t('home:header.about')}</li>
+					<li>{t('header.about')}</li>
 				</ScrollLink>
 				<ScrollLink
 					to='outputs'
 					smooth
 					onClick={() => setIsExpanded(!isExpanded)}
 				>
-					<li>{t('home:header.outputs')}</li>
+					<li>{t('header.outputs')}</li>
 				</ScrollLink>
 				<ScrollLink
 					to='partners'
 					smooth
 					onClick={() => setIsExpanded(!isExpanded)}
 				>
-					<li>{t('home:header.partners')}</li>
+					<li>{t('header.partners')}</li>
 				</ScrollLink>
 				<Link href='/platform' passHref>
-					<li>{t('home:header.platform')}</li>
+					<li>{t('header.platform')}</li>
 				</Link>
 			</ul>
 			<LangSwitcher />
+
+			<a
+				className='surveyLink'
+				href='https://forms.gle/CphVtDuDhVgko1DX8'
+				target='_blank'
+				rel='noreferrer'
+			>
+				{t('header.survey')}
+			</a>
 		</SidebarStyled>
 	);
 };
--- components/Header/Sidebar/SidebarStyled.js
+++ components/Header/Sidebar/SidebarStyled.js
@@ -1,32 +1,43 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export default styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  transform: translateY(-100%);
-  background: ${({ theme }) => theme.colors.darkBlue};
-  height: 100vh;
-  width: 100vw;
-  transition: all 0.8s cubic-bezier(0.6, 0.04, 0.98, 0.335);
-  z-index: -1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  &.expanded {
-    transform: translateX(0);
-  }
-  ul {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: aliceblue;
-    li {
-      font-size: 3rem;
-      margin: 1rem 0;
-    }
-  }
-`
+	position: fixed;
+	top: 0;
+	left: 0;
+	transform: translateY(-100%);
+	background: ${({ theme }) => theme.colors.darkBlue};
+	height: 100vh;
+	width: 100vw;
+	transition: all 0.8s cubic-bezier(0.6, 0.04, 0.98, 0.335);
+	z-index: -1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	&.expanded {
+		transform: translateX(0);
+	}
+	ul {
+		list-style: none;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: aliceblue;
+		li {
+			font-size: 3rem;
+			margin: 1rem 0;
+		}
+	}
+
+	.surveyLink {
+		display: block;
+		padding: 0.5rem 1rem;
+		text-decoration: none;
+		color: white;
+		background-color: ${({ theme }) => theme.colors.blue};
+		font-size: 1.75rem;
+		margin-bottom: 0;
+		margin-top: 3rem;
+	}
+`;
